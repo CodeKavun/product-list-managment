9bab5cc1243f8df0c1d3684ce93c11290f219df0
PanDmytryk <dimacapny22@gmail.com>
2025-05-19 20:30:45 +0200

    Add products page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from .views import *
 
 
+app_name = 'core'
+
 urlpatterns = [
-    path('', hello, name='hello'),
+    path('', RedirectView.as_view(pattern_name='core:products', permanent=False)),
     path('about/', about, name='about'),
-    path('about/core/', about_core, name='core-about')
+    path('about/core/', about_core, name='core-about'),
+
+    path('products/', products, name='products'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,12 +1,44 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from apps.core.models import Product
+
 
 # Create your views here.
 def hello(request):
     return HttpResponse("Hello everyone!")
 
+
+@require_http_methods(['GET', 'POST'])
+def products(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        amount = request.POST.get('amount')
+
+        Product.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            amount=amount
+        )
+
+        return redirect('core:products')
+
+    products_list = Product.objects.all()
+
+    context = {
+        'products': products_list
+    }
+
+    return render(request, 'core/pages/products.html', context)
+
+
 def about(request):
     return render(request, 'about_project.html')
+
 
 def about_core(request):
     return render(request, 'core/about_core.html')
